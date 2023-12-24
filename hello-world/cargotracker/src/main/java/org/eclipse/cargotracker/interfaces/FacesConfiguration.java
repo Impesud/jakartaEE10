be4ac4d1aed42a3ba01df7b436d4f6a72b38e668
Erick <erick.jara@hotmail.it>
2023-12-24 18:46:30 +0100

    Hello Worl web app

---
+++ hello-world/cargotracker/src/main/java/org/eclipse/cargotracker/interfaces/FacesConfiguration.java
@@ -0,0 +1,9 @@
+package org.eclipse.cargotracker.interfaces;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/** Jakarta Faces configuration. * */
+@FacesConfig()
+@ApplicationScoped
+public class FacesConfiguration {}
