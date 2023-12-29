0063567d40b9325cc35134740b524a797243840b
Erick <erick.jara@hotmail.it>
2023-12-29 18:50:27 +0100

    Update Jakarta EE 10 and MySQL

--- jakarta10-mysql/src/main/java/com/impesud/products/ProductRepository.java
+++ jakarta10-mysql/src/main/java/com/impesud/products/ProductRepository.java
@@ -3,7 +3,6 @@ package com.impesud.products;
 import jakarta.inject.Singleton;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -12,11 +11,6 @@ public class ProductRepository {
 
     @PersistenceContext(unitName = "databaseUnit")
     private EntityManager entityManager;
-
-    @Transactional
-    public void save(Product product) {
-        entityManager.persist(product);
-    }
 
     public Product findById(Long id) {
         return entityManager.find(Product.class, id);
